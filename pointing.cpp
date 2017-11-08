f589789f7778d470c91e8ca967f362315f0d3866
edantonio505 <edantonio505@gmail.com>
2017-11-08 14:39:57 -0500

    ch7

---
+++ pointing.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int* paPointer; //declare a pointer
+	int* pScore = 0; //declare and initialize a pointer
+
+	int score = 1000;
+	pScore = &score; //assign pointer pScore address of variable
+
+	cout << "Assigning a &score to pScore\n";
+	cout << "&score is: " << &score << "\n";
+
+	cout << "pScore is: " << pScore << "\n";
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n"; //value pointed to by pointer
+
+	cout << "Adding 500 to score\n";
+	score += 500;
+
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+
+	cout << "Adding 500 to *pScore\n";
+	*pScore += 500;
+	cout << "score is: " << score << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+
+
+	cout << "Assigning &newScore to pScore\n";
+	int newScore = 5000;
+	pScore = &newScore;
+	cout << "&newCore is: " << &newScore << "\n";
+	cout << "pScore is: " << pScore << "\n";
+	cout << "newScore is: " << newScore << "\n";
+	cout << "*pScore is: " << *pScore << "\n\n";
+
+
+	cout << "Assigning &str to pStr\n";
+	string str = "score";
+	string* pStr = &str; //pointer to string object
+
+	cout << "str is " << str << "\n";
+	cout << "*pStr is: " << *pStr << "\n";
+
+	cout << "(*pStr).size() is: " << (*pStr).size() << "\n";
+	cout << "pStr->size() is: " << pStr->size() << "\n";
+	return 0;
+}
