ca3217d0acd61c0d42a9e4542a9f299f7ae0698d
Edgar Velazquez <edantonio505@gmail.com>
2017-11-09 20:29:49 -0500

    things

--- static_critter.cpp
+++ static_critter.cpp
@@ -2,42 +2,49 @@
 
 using namespace std;
 
-class Critter
+class Critter 
 {
-public:
-    static int s_Total;     //static member variable declaration
-                            //total number of Critter objects in existence
-    
-    Critter(int hunger = 0);
-    static int GetTotal();  //static member function prototype
-    
-private:
-    int m_Hunger;
+    public:
+        static int s_Total; //static member declaration
+                            // total number of critters in existence.
+
+        Critter(int hunger = 0); //Constructor function 
+        static int GetTotal();
+
+    private:
+        int m_Hunger;
 };
 
-int Critter::s_Total = 0;   //static member variable initialization
 
-Critter::Critter(int hunger): 
-    m_Hunger(hunger)
+
+int Critter::s_Total = 0; //static member variable initialization
+
+Critter::Critter(int hunger): m_Hunger(hunger)
 {
-    cout << "A critter has been born!" << endl;
-    ++s_Total;
+   cout << "A critter has been born!" << endl;
+   ++s_Total;
 }
 
-int Critter::GetTotal()     //static member function definition
+
+int Critter::GetTotal()
 {
     return s_Total;
 }
 
+
+
+
+
+// main function 
 int main()
 {
     cout << "The total number of critters is: ";
     cout << Critter::s_Total << "\n\n";
-     
-    Critter crit1, crit2, crit3;
-      
+
+    Critter crit1,crit2,crit3; //instatiate 3 critters
+
     cout << "\nThe total number of critters is: ";
     cout << Critter::GetTotal() << "\n";
-    
+
     return 0;
 }
